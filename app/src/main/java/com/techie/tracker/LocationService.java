634c5ad0ac438e5326d7f9d63ebce08946ecfd16
Manoj Chauhan <an.ambitious.techie@gmail.com>
2021-11-28 19:57:27 +0530

    background service created

--- app/src/main/java/com/techie/tracker/LocationService.java
+++ app/src/main/java/com/techie/tracker/LocationService.java
@@ -1,77 +1,138 @@
 package com.techie.tracker;
 
 import android.Manifest;
-import android.app.IntentService;
+import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.IBinder;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicReference;
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDateTime;
 
-public class LocationService extends IntentService {
+public class LocationService extends Service implements LocationListener {
 
-    private FusedLocationProviderClient fusedLocationClient;
+    private LocationManager locationManager;
+    private String provider;
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i("TRACKER","Service Started");
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
 
-    public LocationService() {
-        super("Tracker");
     }
 
     @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
-        File[] drive = this.getApplicationContext().getExternalFilesDirs(null);
-        System.out.println("SDCARDS: " + drive[0]);
+    public void onDestroy() {
+        locationManager.removeUpdates(this);
+        super.onDestroy();
+    }
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.getApplicationContext());
-        File locationFile = new File(drive[0].toString() + "locations.txt");
-        FileWriter writer = null;
+    private void publishCoordinates(Double lat, Double lng){
         try {
-            writer = new FileWriter(locationFile);
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String URL = "http://192.168.1.106:81/record/geo_coordinates";
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("coordinates", lat + "," + lng);
+            jsonBody.put("time", LocalDateTime.now());
+            final String requestBody = jsonBody.toString();
 
-            AtomicReference<String> prevLoc = new AtomicReference<>("");
-        while(true) {
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("VOLLEY", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
 
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                System.out.println("####################No permission################33");
-                return;
-            }
-            final FileWriter writerRef = writer;
-            fusedLocationClient.getLastLocation()
-                    .addOnSuccessListener( (OnSuccessListener<Location>) location -> {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            System.out.println("Current Location:::::::: " + location);
-                            try {
-                                String currentLoc = location.getLatitude() + ":" + location.getLongitude() + "\n";
-//                                if(!currentLoc.equals(prevLoc.get())) {
-                                    writerRef.write(currentLoc);
-//                                }
-                                prevLoc.set(currentLoc);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-        }
-        } catch (IOException e) {
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        // can get more details such as response.headers
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        Log.i("TRACKER", "Location: " + lat + "," + lng);
+//        publishCoordinates(lat, lng);
     }
 }
--- app/src/main/java/com/techie/tracker/MainActivity.java
+++ app/src/main/java/com/techie/tracker/MainActivity.java
@@ -9,12 +9,14 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 
 import com.android.volley.AuthFailureError;
@@ -40,158 +42,19 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity implements LocationListener {
+public class MainActivity extends Activity {
 
-    private LocationManager locationManager;
-    private String provider;
+
     private TextView tv;
-    List<String> locationList = new ArrayList<>();
-    FileWriter writer = null;
 
-    private void publishCoordinates(Double lat, Double lng){
-        try {
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
-            String URL = "http://192.168.1.106:81/record/geo_coordinates";
-            JSONObject jsonBody = new JSONObject();
-            jsonBody.put("coordinates", lat + "," + lng);
-            jsonBody.put("time", LocalDateTime.now());
-            final String requestBody = jsonBody.toString();
 
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    Log.i("VOLLEY", response);
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.e("VOLLEY", error.toString());
-                }
-            }) {
-                @Override
-                public String getBodyContentType() {
-                    return "application/json; charset=utf-8";
-                }
-
-                @Override
-                public byte[] getBody() throws AuthFailureError {
-                    try {
-                        return requestBody == null ? null : requestBody.getBytes("utf-8");
-                    } catch (UnsupportedEncodingException uee) {
-                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
-                        return null;
-                    }
-                }
-
-                @Override
-                protected Response<String> parseNetworkResponse(NetworkResponse response) {
-                    String responseString = "";
-                    if (response != null) {
-                        responseString = String.valueOf(response.statusCode);
-                        // can get more details such as response.headers
-                    }
-                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
-                }
-            };
-
-            requestQueue.add(stringRequest);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.loc);
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-
-
-        Criteria criteria = new Criteria();
-        provider = locationManager.getBestProvider(criteria, false);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        Location location = locationManager.getLastKnownLocation(provider);
-
-
-        if (location != null) {
-            onLocationChanged(location);
-        }
-    }
-
-    /* Request updates at startup */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(provider, 400, 1, this);
-    }
-
-    /* Remove the locationlistener updates when Activity is paused */
-    @Override
-    protected void onPause() {
-        super.onPause();
-        locationManager.removeUpdates(this);
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        double lat = location.getLatitude();
-        double lng = location.getLongitude();
-        String locationString = String.valueOf(lat) + " , " + String.valueOf(lng) + "\n";
-        locationList.add(locationString);
-        File[] drive = this.getApplicationContext().getExternalFilesDirs(null);
-        System.out.println("SDCARDS: " + drive[0]);
-        File locationFile = new File(drive[0].toString() + "/locations.txt");
-
-        try {
-            writer = new FileWriter(locationFile,true);
-            writer.write(locationString);
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String locStr = "";
-        for (String str :
-                locationList) {
-            locStr += str;
-        }
-        tv.setText(locStr);
-
-        publishCoordinates(lat, lng);
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        Toast.makeText(this, "Enabled new provider " + provider,
-                Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        Toast.makeText(this, "Disabled provider " + provider,
-                Toast.LENGTH_SHORT).show();
+        getApplicationContext().startForegroundService(new Intent( this, LocationService.class ) );
     }
 
 }
