e75230db4c69744c272afe1624d105f217f3a187
Manoj Chauhan <manoj@ecomexpress.in>
2023-01-26 22:23:13 +0530

    Buttons made dynamic

--- app/src/main/java/com/samrish/driver/ui/TripDetailActivity.kt
+++ app/src/main/java/com/samrish/driver/ui/TripDetailActivity.kt
@@ -4,13 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 import com.android.volley.toolbox.Volley
 import com.samrish.driver.R
+import com.samrish.driver.models.Trip
 import com.samrish.driver.services.SessionStorage
 import com.samrish.driver.services.TripDetailRequest
 
 class TripDetailActivity : AppCompatActivity() {
+
+    private var checkInButton: AppCompatButton? = null
+    private var departButton: AppCompatButton? = null
+    private var endButton: AppCompatButton? = null
+    private var cancelButton: AppCompatButton? = null
 
     var currentTripCode: String? = null
 
@@ -29,6 +37,10 @@ class TripDetailActivity : AppCompatActivity() {
             currentTripCode = bundle.getString("TRIP_CODE")
         }
         setContentView(R.layout.activity_trip_detail)
+        checkInButton = findViewById<AppCompatButton>(R.id.trip_detail_btn_check_in)
+        departButton = findViewById<AppCompatButton>(R.id.trip_detail_btn_depart)
+        endButton = findViewById<AppCompatButton>(R.id.trip_detail_btn_end)
+        cancelButton = findViewById<AppCompatButton>(R.id.trip_detail_btn_cancel)
         getTripDetail()
     }
 
@@ -44,12 +56,59 @@ class TripDetailActivity : AppCompatActivity() {
 
         val stringRequest = TripDetailRequest(url, hdrs, { response ->
             Log.i("TripDetail", "Trip Detail: $response")
-            findViewById<TextView>(R.id.trip_detail_name).text = response.name
-            findViewById<TextView>(R.id.trip_detail_code).text = response.code
+            showDetails(response)
         }, { error ->
             Log.i("TripDetail", "Request Failed with Error: $error")
         })
         queue.add(stringRequest)
+    }
+
+    private fun showDetails(trip: Trip){
+        findViewById<TextView>(R.id.trip_detail_name).text = trip.name
+        findViewById<TextView>(R.id.trip_detail_code).text = trip.code
+        when(trip.status) {
+            0 -> {
+                findViewById<TextView>(R.id.trip_detail_status).text = "NOT STARTED"
+                checkInButton?.visibility = View.VISIBLE
+                departButton?.visibility = View.GONE
+                endButton?.visibility = View.GONE
+                cancelButton?.visibility = View.VISIBLE
+            }
+            1 -> {
+                findViewById<TextView>(R.id.trip_detail_status).text = "CHECKED-IN"
+                checkInButton?.visibility = View.GONE
+                departButton?.visibility = View.VISIBLE
+                endButton?.visibility = View.VISIBLE
+                cancelButton?.visibility = View.VISIBLE
+            }
+            2 -> {
+                findViewById<TextView>(R.id.trip_detail_status).text = "DEPARTED"
+                checkInButton?.visibility = View.VISIBLE
+                departButton?.visibility = View.GONE
+                endButton?.visibility = View.VISIBLE
+                cancelButton?.visibility = View.VISIBLE
+            }
+            3 -> {
+                findViewById<TextView>(R.id.trip_detail_status).text = "ENDED"
+            }
+            4 -> {
+                findViewById<TextView>(R.id.trip_detail_status).text = "CANCELLED"
+            }
+            else -> {
+                findViewById<TextView>(R.id.trip_detail_status).text = "ERROR"
+            }
+        }
+    }
+
+    private fun generateStatusString(status: Int): String{
+        return when (status) {
+            0 -> "NOT STARTED"
+            1 -> "CHECKED_IN"
+            2 -> "DEPARTED"
+            3 -> "ENDED"
+            4 -> "CANCELLED"
+            else -> "ERROR"
+        }
     }
 
 }
