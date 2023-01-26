88ee58cc447c4f841c0ff80faf8421f1597448e4
Manoj Chauhan <manoj@ecomexpress.in>
2023-01-26 09:43:40 +0530

    On click implemented

--- app/src/main/java/com/samrish/driver/LoginActivity.kt
+++ app/src/main/java/com/samrish/driver/LoginActivity.kt
@@ -1,33 +1,31 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.samrish.driver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.RecyclerView
+import android.util.Log
+import android.view.View
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 
-const val FLOWER_ID = "flower id"
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
-class LoginActivity : AppCompatActivity() {
+    private lateinit var editTextUsername: AppCompatEditText;
+    private lateinit var editTextPassword: AppCompatEditText;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-//        val recyclerView: RecyclerView = findViewById(R.id.recycler_view);
+        editTextUsername = findViewById(R.id.login_input_username)
+        editTextPassword = findViewById(R.id.login_input_password);
+
+        var buttonLogin: AppCompatButton = findViewById(R.id.login_btn_login);
+        buttonLogin.setOnClickListener(this);
+    }
+
+    override fun onClick(p0: View?) {
+        var username: String = editTextUsername.text.toString()
+        var password: String = editTextPassword.text.toString()
+        Log.i("Login", "Username: $username  Password: $password");
     }
 
 }
