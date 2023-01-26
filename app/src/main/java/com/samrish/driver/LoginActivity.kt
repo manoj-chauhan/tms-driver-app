3aecd05b24cef487d61db53bbcda53c7c646dde9
Manoj Chauhan <manoj@ecomexpress.in>
2023-01-26 11:12:09 +0530

    Network permission added

--- app/src/main/java/com/samrish/driver/LoginActivity.kt
+++ app/src/main/java/com/samrish/driver/LoginActivity.kt
@@ -6,6 +6,9 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -22,9 +25,22 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(p0: View?) {
-        var username: String = editTextUsername.text.toString()
-        var password: String = editTextPassword.text.toString()
+        val username: String = editTextUsername.text.toString()
+        val password: String = editTextPassword.text.toString()
         Log.i("Login", "Username: $username  Password: $password");
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://www.google.com"
+
+        val stringRequest = StringRequest(Request.Method.GET, url, {
+                response -> Log.i("Login", "Response: $response")
+            }, {
+                Log.i("Login", "Request Failed")
+            }
+        )
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
     }
 
 }
