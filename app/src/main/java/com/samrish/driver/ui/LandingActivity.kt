92f3c4c22a17a568cfbfd4a703bb593bc048144e
Manoj Chauhan <an.ambitious.techie@gmail.com>
2023-03-08 21:40:50 +0530

    navigation initiated

---
+++ app/src/main/java/com/samrish/driver/ui/LandingActivity.kt
@@ -0,0 +1,14 @@
+package com.samrish.driver.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.samrish.driver.R
+
+class LandingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+
+}
---
+++ app/src/main/java/com/samrish/driver/ui/fragments/ExampleFragment.kt
@@ -0,0 +1,8 @@
+package com.samrish.driver.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.samrish.driver.R
+
+class ExampleFragment: Fragment(R.layout.fragment_example) {
+
+}
