610a73e0cdbdc4f8d8d7764379225593ac970f4f
Manoj Chauhan <an.ambitious.techie@gmail.com>
2023-03-10 18:18:50 +0530

    ProfileFragment.kt modified

--- app/src/main/java/com/samrish/driver/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/samrish/driver/ui/fragments/ProfileFragment.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.material3.Text;
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
 
 class ProfileFragment : Fragment() {
@@ -24,10 +33,28 @@ class ProfileFragment : Fragment() {
             )
 
             setContent {
-                Row {
-                    Text("Hello Compose!")
-                }
+                PersonalDetail()
             }
         }
     }
+}
+
+@Composable
+fun PersonalDetail() {
+    Card(modifier = Modifier.padding(20.dp)) {
+        Column(modifier = Modifier.padding(20.dp)) {
+            Text(
+                text = "Manoj Chauhan",
+                modifier = Modifier.padding(10.dp),
+                color = Color(0xFFE90202)
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun PersonalDetailPreview() {
+    PersonalDetail()
 }
