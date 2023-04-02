15dab887b8b1277ed8e3bc1f3f06a78337e2b45c
Manoj Chauhan <an.ambitious.techie@gmail.com>
2023-04-02 22:21:46 +0530

    custom dialog

---
+++ app/src/main/java/com/samrish/driver/ui/components/CheckInDialog.kt
@@ -0,0 +1,75 @@
+package com.samrish.driver.ui.components
+
+import android.R
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.Money
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.samrish.driver.models.Schedule
+
+
+// Reference: https://medium.com/@manojbhadane/android-custom-dialog-using-jetpack-compose-954d83e55af7
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CheckInDialog(
+    schedules: List<Schedule>,
+    value: String,
+    setShowDialog: (Boolean) -> Unit,
+    setValue: (String) -> Unit
+) {
+
+    val txtFieldError = remember { mutableStateOf("") }
+    val txtField = remember { mutableStateOf(value) }
+
+    Dialog(onDismissRequest = { setShowDialog(false) }) {
+        Surface(
+            shape = RoundedCornerShape(16.dp),
+            color = Color.White
+        ) {
+            Box(
+                contentAlignment = Alignment.Center
+            ) {
+                Column() {
+                        for (schedule in schedules) {
+                            Text(text = schedule.placeName + "(" + schedule.placeCode + ")")
+                        }
+
+                    Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+                        Button(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(50.dp),
+                            onClick = {},
+                            shape = RoundedCornerShape(50.dp)
+                        ) {
+                            Text(text = "Done")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
