b4316a3f5b513903906078d286e7a1e846692148
Manoj Chauhan <an.ambitious.techie@gmail.com>
2023-04-08 18:46:14 +0530

    assignment history made scrollable

--- app/src/main/java/com/samrish/driver/ui/pages/History.kt
+++ app/src/main/java/com/samrish/driver/ui/pages/History.kt
@@ -2,6 +2,7 @@ package com.samrish.driver.ui.pages
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -90,11 +91,13 @@ fun TripDetail(oldAssignment: OldAssignment) {
 
 @Composable
 fun TripList(oldAssignmentList: List<OldAssignment>) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
     ) {
-        oldAssignmentList.forEach { oldAssignment -> TripDetail(oldAssignment) }
+        items(
+            oldAssignmentList.size
+        ) {
+            TripDetail(oldAssignmentList[it])
+        }
     }
 }
