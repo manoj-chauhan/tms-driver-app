49d2b3b070dc565c21e9eb966a05e6c75ae08991
Manoj Chauhan <manoj@ecomexpress.in>
2023-10-12 22:53:46 +0530

    testing of json serialization using Moshi library

--- app/src/main/java/com/samrish/driver/viewmodels/VehicleAssignment.kt
+++ app/src/main/java/com/samrish/driver/viewmodels/VehicleAssignment.kt
@@ -10,7 +10,9 @@ import com.github.kittinunf.fuel.moshi.moshiDeserializerOf
 import com.samrish.driver.R
 import com.samrish.driver.services.getAccessToken
 import com.samrish.driver.services.vehicleDetails
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -38,18 +40,25 @@ class VehicleAssignmentViewModel : ViewModel() {
     val currentAssignment: StateFlow<VehicleAssignment?> = _currentAssignment.asStateFlow()
 
     fun fetchAssignmentDetail(context: Context) {
-        Log.i("Fuel", "Going for fuel")
         viewModelScope.launch(Dispatchers.IO) {
-            Log.i("Fuel", "Requesting using fuel")
             val vehicleAssignmentUrl = context.resources.getString(R.string.url_vehicle_assignment)
             getAccessToken(context)?.let {
-                Log.e("Fuel", "Token $it")
                 val (request1, response1, result1) = vehicleAssignmentUrl.httpGet()
                     .authentication().bearer(it)
                     .responseObject(moshiDeserializerOf(VehicleAssignment::class.java))
 
                 result1.fold(
-                    { vehicleAssignment -> _currentAssignment.update { _ -> vehicleAssignment } },
+                    { vehicleAssignment ->
+                        _currentAssignment.update { _ ->
+                            ////The below code is not supposed to be here, it is placed here just for testing serialisation using Moshi library
+                            val moshi = Moshi.Builder().build()
+                            val adapter: JsonAdapter<VehicleAssignment> = moshi.adapter(VehicleAssignment::class.java)
+                            val json: String? = adapter.toJson(vehicleAssignment)
+                            Log.i("Fuel","Generated: $json")
+                            ////The above code is not supposed to be here, it is placed here just for testing serialisation using Moshi library
+                            vehicleAssignment
+                        }
+                    },
                     { error ->
                         Log.e(
                             "Fuel",
@@ -58,7 +67,6 @@ class VehicleAssignmentViewModel : ViewModel() {
                     }
                 )
             }
-            Log.e("Fuel", "End of the call")
         }
     }
 }
