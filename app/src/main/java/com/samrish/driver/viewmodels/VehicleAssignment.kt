d90564ebc0fdc06af3d00570e460ec772836d1d0
Manoj Chauhan <manoj@ecomexpress.in>
2023-10-12 20:56:28 +0530

    First request made using Fuel

--- app/src/main/java/com/samrish/driver/viewmodels/VehicleAssignment.kt
+++ app/src/main/java/com/samrish/driver/viewmodels/VehicleAssignment.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.github.kittinunf.fuel.core.extensions.authentication
 import com.github.kittinunf.fuel.httpGet
+import com.samrish.driver.R
+import com.samrish.driver.services.getAccessToken
 import com.samrish.driver.services.vehicleDetails
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -14,40 +17,63 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 data class VehicleAssignment(
-    val vehicleId : Int,
-    val vehicleNumber : String,
-    val companyId : Int,
-    val companyName : String,
-    val assignerName : String,
-    val assignedAt : String,
-    val vehicleSize : Int,
-    val model : String,
-    val brand : String,
-    val fuelType : String
-    
-    
+    val vehicleId: Int,
+    val vehicleNumber: String,
+    val companyId: Int,
+    val companyName: String,
+    val assignerName: String,
+    val assignedAt: String,
+    val vehicleSize: Int,
+    val model: String,
+    val brand: String,
+    val fuelType: String
+
+
 )
 
 class VehicleAssignmentViewModel : ViewModel() {
 
-    private val _currentAssignment:MutableStateFlow<VehicleAssignment?> = MutableStateFlow(null)
+    private val _currentAssignment: MutableStateFlow<VehicleAssignment?> = MutableStateFlow(null)
     val currentAssignment: StateFlow<VehicleAssignment?> = _currentAssignment.asStateFlow()
 
-    fun fetchAssignmentDetail(context:Context) {
+    fun fetchAssignmentDetail(context: Context) {
         Log.i("Fuel", "Going for fuel")
         viewModelScope.launch(Dispatchers.IO) {
             Log.i("Fuel", "Requesting using fuel")
-            val (request, response, result) = "https://publicobject.com/helloworld.txt".httpGet().response()
-            result.fold(
-                {_ -> Log.i("Fuel", "Response + ${response.body().asString("text/plain")}")},
-                {error -> Log.e("Fuel", "Error ${error.response.body().asString("text/plain")}")}
-            )
+            val vehicleAssignmentUrl = context.resources.getString(R.string.url_vehicle_assignment)
+            getAccessToken(context)?.let {
+                Log.e("Fuel", "Token $it")
+                val (request1, response1, result1) = vehicleAssignmentUrl.httpGet()
+                    .authentication().bearer(it)
+                    .response()
+
+                result1.fold(
+                    { _ -> Log.i("Fuel", "Response + ${response1.body().asString("text/plain")}") },
+                    { error ->
+                        Log.e(
+                            "Fuel",
+                            "Error ${error.response.body().asString("text/plain")}"
+                        )
+                    }
+                )
+            }
             Log.e("Fuel", "End of the call")
         }
 
         vehicleDetails(context, onVehicleDetailFetched = {
             _currentAssignment.update { assignment ->
-                VehicleAssignment(it.vehicleId, it.vehicleNumber, it.companyId, it.companyName, it.assignerName,it.assignedAt, it.vehicleSize, it.model, it.brand, it.fuelType )
+                VehicleAssignment(
+                    it.vehicleId,
+                    it.vehicleNumber,
+                    it.companyId,
+                    it.companyName,
+                    it.assignerName,
+                    it.assignedAt,
+                    it.vehicleSize,
+                    it.model,
+                    it.brand,
+                    it.fuelType
+                )
             }
         })
     }
