45679cca44f12418888ee55613574ba9745c8ec3
Manoj Chauhan <manoj@ecomexpress.in>
2023-10-13 09:33:08 +0530

    coroutine for getting trip assignments

--- app/src/main/java/com/samrish/driver/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/samrish/driver/viewmodels/HomeViewModel.kt
@@ -8,11 +8,12 @@ import com.github.kittinunf.fuel.core.extensions.authentication
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.moshi.moshiDeserializerOf
 import com.samrish.driver.R
+import com.samrish.driver.models.AssignedVehicle
 import com.samrish.driver.services.getAccessToken
-import com.samrish.driver.services.vehicleDetails
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -34,13 +35,29 @@ data class VehicleAssignment(
     val fuelType: String
 )
 
+@JsonClass(generateAdapter = true)
+data class TripsAssigned(
+    var tripCode: String,
+    var tripName:String,
+    var status: String,
+    var label: String,
+    var companyName: String,
+    var companyCode: String,
+    var operatorCompanyName: String,
+    var operatorCompanyCode: String,
+    var operatorCompanyId: Int,
+    var tripDate: String,
+    var tripId: Int
+)
+
+
 class VehicleAssignmentViewModel : ViewModel() {
 
     private val _currentAssignment: MutableStateFlow<VehicleAssignment?> = MutableStateFlow(null)
     val currentAssignment: StateFlow<VehicleAssignment?> = _currentAssignment.asStateFlow()
 
     fun fetchAssignmentDetail(context: Context) {
-        viewModelScope.launch(Dispatchers.IO) {
+        val routine1 = viewModelScope.launch(Dispatchers.IO) {
             val vehicleAssignmentUrl = context.resources.getString(R.string.url_vehicle_assignment)
             getAccessToken(context)?.let {
                 val (request1, response1, result1) = vehicleAssignmentUrl.httpGet()
@@ -52,9 +69,10 @@ class VehicleAssignmentViewModel : ViewModel() {
                         _currentAssignment.update { _ ->
                             ////The below code is not supposed to be here, it is placed here just for testing serialisation using Moshi library
                             val moshi = Moshi.Builder().build()
-                            val adapter: JsonAdapter<VehicleAssignment> = moshi.adapter(VehicleAssignment::class.java)
+                            val adapter: JsonAdapter<VehicleAssignment> =
+                                moshi.adapter(VehicleAssignment::class.java)
                             val json: String? = adapter.toJson(vehicleAssignment)
-                            Log.i("Fuel","Generated: $json")
+                            Log.i("Fuel", "Generated: $json")
                             ////The above code is not supposed to be here, it is placed here just for testing serialisation using Moshi library
                             vehicleAssignment
                         }
@@ -68,5 +86,39 @@ class VehicleAssignmentViewModel : ViewModel() {
                 )
             }
         }
+
+        val routine2 = viewModelScope.launch(Dispatchers.IO) {
+            val tripAssignmentUrl = context.resources.getString(R.string.url_trips_list)
+            getAccessToken(context)?.let {
+
+                val assignedTripType = Types.newParameterizedType(MutableList::class.java, TripsAssigned::class.java)
+                val adapter: JsonAdapter<List<AssignedVehicle>> = Moshi.Builder().build().adapter(assignedTripType)
+
+
+                val (request1, response1, result1) = tripAssignmentUrl.httpGet()
+                    .authentication().bearer(it)
+                    .responseObject(moshiDeserializerOf(adapter))
+
+                result1.fold(
+                    {
+                        tripAssignments -> Log.i("Fuel", "$tripAssignments")
+                    },
+                    { error ->
+                        Log.e(
+                            "Fuel",
+                            "Error $error"
+                        )
+                    }
+                )
+            }
+        }
+        viewModelScope.launch(Dispatchers.IO) {
+            routine1.join()
+            routine2.join()
+
+            Log.i("Fuel", "All Coroutines Finished!")
+        }
+
+
     }
 }
