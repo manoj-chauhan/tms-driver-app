0e181597cbf2be5d768d2d0df30aab1c67505295
Akhilesh2000 <yogiakhilesh80@gmail.com>
2024-01-30 19:50:36 +0530

    maps implemented

--- app/src/parent/java/driver/ui/pages/MapsActivity.kt
+++ app/src/parent/java/driver/ui/pages/MapsActivity.kt
@@ -1,191 +1,186 @@
 package driver.ui.pages
 
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Polyline
+import com.google.maps.android.compose.rememberCameraPositionState
 import dagger.hilt.android.AndroidEntryPoint
-import driver.models.ProcessedPoints
-import driver.models.point
 import driver.ui.viewmodels.parentTripAssigned
 
+val indiaState = LatLng(20.5937, 70.9629)
+val defaultcameraPosition = CameraPosition.fromLatLngZoom(indiaState, 4f)
+
 @AndroidEntryPoint
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-    private lateinit var mMap: GoogleMap
-
-    private lateinit var mapView: MapView
-    private lateinit var googleMap: GoogleMap
+class MapsActivity : AppCompatActivity() {
     private var operatorId: Int = 0
-    private var tripCode: String=""
-
+    private var tripCode: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         operatorId = intent.getIntExtra("operatorId", 0)
-
         tripCode = intent.getStringExtra("tripCode").toString()
+
         super.onCreate(savedInstanceState)
 
         setContent {
             Surface(
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier.fillMaxSize(),
             ) {
-                MapViewContent()
-                Log.d("Fg","$tripCode, $operatorId")
+                val cameraPositionState = rememberCameraPositionState {
+                    position = defaultcameraPosition
+                }
+                Box(modifier = Modifier.height(300.dp)) {
+                    GoogleMapView(
+                        modifier = Modifier.fillMaxSize(),
+                        cameraPositionState = cameraPositionState,
+                        operatorId = operatorId,
+                        tripCode = tripCode,
+                        onMapLoaded = {}
+                    )
+                }
             }
         }
-    }
-
-
-
-    @Composable
-    private fun MapViewContent(vm: parentTripAssigned = hiltViewModel()) {
-        val context = LocalContext.current
-        vm.fetchTripRouteCoordinates(context = context,operatorId, tripCode)
-//        vm.fetchTripProcessedCoordinates(context, operatorId, tripCode)
-        vm.fetchTripProcessedCoordinates(context, 1, "3838")
-
-        val tripRoute by vm.points.collectAsStateWithLifecycle()
-        val tripProcessCoord by vm.processedpoints.collectAsStateWithLifecycle()
-
-        mapView = rememberMapViewWithLifecycle()
-        AndroidView(factory = { mapView }) { map ->
-            tripRoute?.let { tripProcessCoord?.let { it1 -> onMapReady(googleMap, it, it1) } }
-           map.getMapAsync { googleMap ->
-                this@MapsActivity.googleMap = googleMap
-                onMapReady(googleMap)
-            }
-        }
-    }
-
-    @Composable
-    fun rememberMapViewWithLifecycle(): MapView {
-        val context = LocalContext.current
-        val mapView = remember {
-            MapView(context).apply {
-                onCreate(null)
-            }
-        }
-
-        DisposableEffect(context) {
-            mapView.onResume()
-
-            onDispose {
-                mapView.onDestroy()
-            }
-        }
-
-        return mapView
-    }
-
-    override fun onResume() {
-        super.onResume()
-        super.onResume()
-        if (::mapView.isInitialized) {
-            mapView.onResume()
-        }    }
-
-    override fun onPause() {
-        super.onPause()
-        if (::mapView.isInitialized) {
-            mapView.onPause()
-        }    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        if (::mapView.isInitialized) {
-            mapView.onSaveInstanceState(outState)
-        }    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-
-    }
-    private fun onMapReady(googleMap: GoogleMap, routePoints: List<point>, processedPoints: List<ProcessedPoints>) {
-        Log.d("TAG", "onMapReady: $routePoints")
-        drawRoute(googleMap, routePoints, processedPoints)
-    }
-
-    private fun getRoutePoints(tripCoor:List<point>): List<point> {
-        val serverResponse = tripCoor
-
-        return getRoutePoints(serverResponse)
-    }
-
-}
-
-private fun drawRoute(googleMap: GoogleMap, routePoints: List<point>?, processedPoints: List<ProcessedPoints>) {
-    if (routePoints.isNullOrEmpty()) {
-        return
-    }
-
-    if (processedPoints.isEmpty()) {
-        return
-    }
-
-    Log.d("TAG", "drawRoute: $processedPoints")
-
-    googleMap.clear()
-
-    val polylineOptions = PolylineOptions()
-        .width(5f)
-        .color(Color.GRAY)
-
-    val processedLineOption = PolylineOptions()
-        .width(5f)
-        .color(Color.BLUE)
-
-    val firstPoint = routePoints.first()
-    val lastPoint = routePoints.last()
-    val firstLatLng = LatLng(firstPoint.latitude, firstPoint.longitude)
-    val lastLatLng = LatLng(lastPoint.latitude, lastPoint.longitude)
-    googleMap.addMarker(MarkerOptions().position(firstLatLng))
-    googleMap.addMarker(MarkerOptions().position(lastLatLng))
-
-    for (point in routePoints) {
-        val latLng = LatLng(point.latitude, point.longitude)
-        polylineOptions.add(latLng)
-    }
-    for (point in processedPoints) {
-        val latLng = LatLng(point.latitude, point.longitude)
-        processedLineOption.add(latLng)
-    }
-
-    googleMap.addPolyline(polylineOptions)
-    googleMap.addPolyline(processedLineOption)
-
-
-    val bounds = LatLngBounds.builder()
-    for (point in routePoints) {
-        bounds.include(LatLng(point.latitude, point.longitude))
-    }
-    val processedBounds = LatLngBounds.builder()
-    for (point in processedPoints) {
-        processedBounds.include(LatLng(point.latitude, point.longitude))
-    }
-    if(!processedPoints.isNullOrEmpty()) {
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(processedBounds.build(), 50))
-    }else{
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 50))
-
     }
 }
 
+@Composable
+fun GoogleMapView(
+    modifier: Modifier,
+    cameraPositionState: CameraPositionState,
+    operatorId: Int,
+    tripCode: String,
+    onMapLoaded: () -> Unit,
+    vm: parentTripAssigned = hiltViewModel()
+
+) {
+    val context = LocalContext.current
+    vm.fetchTripRouteCoordinates(context = context, operatorId, tripCode)
+    vm.fetchTripProcessedCoordinates(context, 1, "3838")
+
+    val tripRoute by vm.points.collectAsStateWithLifecycle()
+    val tripProcessCoord by vm.processedpoints.collectAsStateWithLifecycle()
+
+    Log.d("fetch", "GoogleMapView: $tripRoute")
+    Log.d("fetch", "GoogleMapView: $tripProcessCoord")
+    val routePoints: List<LatLng> =
+        tripRoute?.map { LatLng(it.latitude, it.longitude) } ?: emptyList()
+    val processedPoints: List<LatLng> =
+        tripProcessCoord?.map { LatLng(it.latitude, it.longitude) } ?: emptyList()
+
+    process(routePoints, processedPoints, cameraPositionState, onMapLoaded = {}, )
+//    val firstPoint = routePoints.firstOrNull()
+//    val lastPoint = routePoints.last()
+//    val firstLatLng = LatLng(firstPoint.latitude, firstPoint.longitude)
+//    val lastLatLng = LatLng(lastPoint.latitude, lastPoint.longitude)
+//    val mapUiproperties by remember {
+//        mutableStateOf(
+//            MapProperties(
+//                mapType = MapType.NORMAL
+//            )
+//        )
+//    }
+//
+//    val mapUiSetting by remember {
+//        mutableStateOf(
+//            MapUiSettings(
+//                compassEnabled = false
+//            )
+//        )
+//    }
+
+//    cameraPositionState.position = CameraPosition.Builder()
+//        .target(LatLngBounds.builder().include(firstPoint).include(lastPoint).build().center)
+//        .zoom(12f)
+//        .build()
+
+//    GoogleMap(
+//        modifier = Modifier,
+//        onMapLoaded = onMapLoaded,
+//        cameraPositionState = cameraPositionState,
+//        uiSettings = mapUiSetting,
+//        properties = mapUiproperties
+//    )
+//    {
+//        Polyline(
+//            points = routePoints,
+//            color = Color.Gray,
+//            width = 5f
+//        )
+//
+//        Polyline(
+//            points = processedPoints,
+//            color = Color.Blue,
+//            width = 10f
+//        )
+//
+//        if (firstPoint != null) {
+//            Marker(state = rememberMarkerState(position = firstPoint))
+//        }
+//
+//    }
+
+}
+@Composable
+fun process(routePoints: List<LatLng>, processedPoints: List<LatLng>, cameraPositionState: CameraPositionState, onMapLoaded: () -> Unit) {
+    val mapUiproperties by remember {
+        mutableStateOf(
+            MapProperties(
+                mapType = MapType.NORMAL
+            )
+        )
+    }
+
+
+    val mapUiSetting by remember {
+        mutableStateOf(
+            MapUiSettings(
+                compassEnabled = false
+            )
+        )
+    }
+
+
+    GoogleMap(
+        modifier = Modifier,
+        onMapLoaded = onMapLoaded,
+        cameraPositionState = cameraPositionState,
+        uiSettings = mapUiSetting,
+        properties = mapUiproperties
+    )
+    {
+        Polyline(
+            points = routePoints,
+            color = Color.Gray,
+            width = 5f
+        )
+
+        Polyline(
+            points = processedPoints,
+            color = Color.Blue,
+            width = 10f
+        )
+
+    }
+}
