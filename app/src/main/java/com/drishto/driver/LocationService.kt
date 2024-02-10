0906fff1103cb2574954fdf4936c64afe8674997
Akhilesh2000 <yogiakhilesh80@gmail.com>
2024-02-10 10:32:25 +0530

    location service code changed and tested

--- app/src/main/java/com/drishto/driver/LocationService.kt
+++ app/src/main/java/com/drishto/driver/LocationService.kt
@@ -224,9 +224,6 @@ class LocationService : Service(), LocationListener {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
             fusedLocationClient.requestLocationUpdates(lr, locationCallback, Looper.getMainLooper())
