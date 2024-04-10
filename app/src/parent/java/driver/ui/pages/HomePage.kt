cad977e547e3b9e04cf7957455b1064d3adafa0a
Akhilesh2000 <yogiakhilesh80@gmail.com>
2024-04-10 18:07:21 +0530

    school name, profile and location added in the front page

--- app/src/parent/java/driver/ui/pages/HomePage.kt
+++ app/src/parent/java/driver/ui/pages/HomePage.kt
@@ -1,7 +1,9 @@
 package driver.ui.pages
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
@@ -40,12 +43,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.drishto.driver.R
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
@@ -147,7 +156,7 @@ fun HomePage() {
 
                 )
             }
-            Spacer(modifier = Modifier.height(2.dp))
+            Spacer(modifier = Modifier.height(1.dp))
 
             val tabItems = listOf(
                 tabItem("All"), tabItem("School"), tabItem("Class")
@@ -196,20 +205,82 @@ fun HomePage() {
                             selectedTabIndex = pagerState.currentPage
                         }
 
-                        Spacer(modifier = Modifier
-                            .height(10.dp)
-                            .background(Color.LightGray))
+                        Spacer(
+                            modifier = Modifier
+                                .height(10.dp)
+                                .background(Color.LightGray)
+                        )
 
                         HorizontalPager(
                             state = pagerState, modifier = Modifier
                                 .fillMaxWidth()
                         ) { index ->
+                            Box(modifier = Modifier.fillMaxSize()) {
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .background(Color.White),
+                                ) {
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(12.dp)
+                                    ) {
+                                        Column(modifier = Modifier) {
+                                            Row(modifier = Modifier.fillMaxWidth()) {
+                                                Box(
+                                                    modifier = Modifier
+                                                        .background(
+                                                            Color.White,
+                                                            shape = CircleShape
+                                                        )
+                                                        .size(44.dp)
+                                                ) {
+                                                    Image(
+                                                        painter = painterResource(id = R.drawable.atul),
+                                                        contentDescription = "",
+                                                        modifier = Modifier
+                                                            .width(200.dp)
+                                                            .height(200.dp)
+                                                            .clip(CircleShape)
+                                                            .border(
+                                                                width = 0.dp,
+                                                                Color.White,
+                                                                shape = CircleShape
+                                                            ),
+                                                        contentScale = ContentScale.FillBounds
+                                                    )
+                                                }
+                                                Spacer(modifier = Modifier.width(14.dp))
 
-                            Box(
-                                modifier = Modifier.fillMaxWidth()
-                                    .background(Color.White),
-                            ) {
-                                Text(text = tabItems[index].title)
+                                                Column(modifier = Modifier.fillMaxWidth()) {
+                                                    Box(modifier = Modifier.fillMaxWidth()) {
+                                                        Text(
+                                                            text = "Delhi Public School",
+                                                            style = TextStyle(
+                                                                fontSize = 16.sp,
+                                                                fontFamily = FontFamily.SansSerif
+                                                            )
+                                                        )
+                                                    }
+                                                    Box(modifier = Modifier.fillMaxWidth()) {
+                                                        Text(
+                                                            text = "Sonipath, Haryana",
+                                                            style = TextStyle(
+                                                                fontSize = 14.sp,
+                                                                fontFamily = FontFamily.SansSerif,
+                                                                color = Color.Gray
+                                                            )
+                                                        )
+                                                    }
+                                                }
+
+                                            }
+                                        }
+
+                                    }
+//                                Text(text = tabItems[index].title)
+                                }
                             }
                         }
                     }
