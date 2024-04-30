0669de08485e85a57e75b175ce19a13248788b28
Akhilesh2000 <yogiakhilesh80@gmail.com>
2024-04-30 16:55:52 +0530

    top bar made for events screen.

--- app/src/parent/java/driver/ui/pages/SavedEvents.kt
+++ app/src/parent/java/driver/ui/pages/SavedEvents.kt
@@ -1,39 +1,39 @@
 package driver.ui.pages
 
-import android.text.Layout.Alignment
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.windowInsetsEndWidth
-import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.graphics.vector.ImageVector
-import kotlinx.coroutines.launch
-
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.*
-
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.*
-
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 enum class HomeTabs(
 
@@ -59,12 +59,37 @@ fun SavedEvents() {
 
 
     Scaffold(
-        topBar = { TopAppBar(title = { Text(text = "Events") }) }
+        topBar = { Row(
+            modifier = Modifier
+                .padding(top = 4.dp)
+                .fillMaxWidth().height(40.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ){
+            Box(modifier = Modifier.width(60.dp).align(Alignment.CenterVertically)) {
+                IconButton(modifier = Modifier.size(25.dp).align(Alignment.Center), onClick = {
+
+                }) {
+                    Icon(
+                        modifier = Modifier,
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            }
+
+            Box(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "Events", style =  TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
+            }
+
+
+        } }
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(top = it.calculateTopPadding())
+                .padding(paddingValues = it)
         ) {
             TabRow(
                 selectedTabIndex = selectedTabIndex,
@@ -73,6 +98,7 @@ fun SavedEvents() {
                 HomeTabs.entries.forEachIndexed { index, currentTab ->
                     Tab(
                         selected = selectedTabIndex == index,
+                        modifier = Modifier.height(40.dp),
                         selectedContentColor = MaterialTheme.colorScheme.primary,
                         unselectedContentColor = MaterialTheme.colorScheme.outline,
                         onClick = {
@@ -102,9 +128,6 @@ fun SavedEvents() {
 
 
 }
-
-
-
 
 
 
