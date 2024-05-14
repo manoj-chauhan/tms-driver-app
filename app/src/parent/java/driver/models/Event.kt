7ad0c536310bbfb1fb1595818d76456d8c3a94f6
anirudhshar <anirudhsharma0702@gmail.com>
2024-05-14 13:19:21 +0530

    file names refactored

--- app/src/parent/java/driver/models/Event.kt
+++ app/src/parent/java/driver/models/Event.kt
@@ -41,3 +41,34 @@ data class Image(
 
 
 
+@JsonClass(generateAdapter = true)
+data class EventRegistration(
+    val title: String,
+    val description: String,
+    val location: String,
+    val scope: String,
+    val dateOfEvent: String?,
+    val timeOfEvent:  String?,
+    val coverImage:ImagesInfo?,
+    val descriptionImage: List<ImagesInfo?>,
+    val institute:  String
+)
+
+@JsonClass(generateAdapter = true)
+data class locationInfo(
+
+    val latitude:String,
+    val longitude:String
+
+)
+@JsonClass(generateAdapter = true)
+data class ImagesInfo(
+    val type :  String,
+    val mediaId :  String,
+    val caption :String?
+
+)
+
+
+
+
