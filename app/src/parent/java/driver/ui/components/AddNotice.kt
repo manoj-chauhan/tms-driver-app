4204529d2fdf24f903b9e3b2dc40e313f29a56b4
anirudhshar <anirudhsharma0702@gmail.com>
2024-05-24 18:00:50 +0530

    time picker applied but small issue

--- app/src/parent/java/driver/ui/components/AddNotice.kt
+++ app/src/parent/java/driver/ui/components/AddNotice.kt
@@ -1,7 +1,8 @@
 package driver.ui.components
 
-//import kotlinx.coroutines.flow.internal.NoOpContinuation.context
+
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Context
 import android.net.Uri
 import android.util.Log
@@ -9,6 +10,7 @@ import android.widget.DatePicker
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -21,7 +23,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Schedule
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -44,6 +48,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import driver.models.ImagesInfo
 import driver.ui.viewmodels.EventsViewModel
 import driver.ui.viewmodels.NoticesViewModel
@@ -61,11 +67,10 @@ fun AddNoticeEvent() {
     var selectedDate by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
     var selectedNoticeUri by remember { mutableStateOf<Uri?>(null) }
+    var selectedTime by remember { mutableStateOf("") }
 
     val interactionSource = remember { MutableInteractionSource() }
     val isPressed: Boolean by interactionSource.collectIsPressedAsState()
-
-
 
     val calendar = Calendar.getInstance()
     val year = calendar.get(Calendar.YEAR)
@@ -78,13 +83,49 @@ fun AddNoticeEvent() {
     val noticeMedia by eventsViewModel.postDetails.collectAsStateWithLifecycle()
     var notice = remember { mutableStateOf<ImagesInfo?>(null) }
 
+    val selectedTimeText = remember { mutableStateOf("") }
+
+    val timePickerDialog = remember { mutableStateOf(false) }
+
+    val mContext = LocalContext.current
+
+
+    val mYear: Int
+    val mMonth: Int
+    val mDay: Int
+
+
+    val mCalendar = Calendar.getInstance()
+
+
+    mYear = mCalendar.get(Calendar.YEAR)
+    mMonth = mCalendar.get(Calendar.MONTH)
+    mDay = mCalendar.get(Calendar.DAY_OF_MONTH)
+
+    mCalendar.time = Date()
+
+
+    val mDate = remember { mutableStateOf("") }
+
+
+    val mDatePickerDialog = DatePickerDialog(
+        mContext,
+        { _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
+            mDate.value = "$mDayOfMonth/${mMonth+1}/$mYear"
+        }, mYear, mMonth, mDay
+    )
+
+    val onTimeSetListener = TimePickerDialog.OnTimeSetListener { _, selectedHourOfDay, selectedMinute ->
+        selectedTime = "$selectedHourOfDay:$selectedMinute"
+    }
+
     LaunchedEffect(noticeMedia) {
-        if (noticeMedia!=null) {
-            notice.value= ImagesInfo(
+        if (noticeMedia != null) {
+            notice.value = ImagesInfo(
                 type = when (noticeMedia!!.second) {
                     "video/mp4" -> "Video"
                     "image/jpeg", "image/png" -> "Image"
-                    "pdf/pdf"->"Pdf"
+                    "pdf/pdf" -> "Pdf"
                     else -> "Unknown"
                 },
                 mediaId = noticeMedia!!.first,
@@ -95,7 +136,7 @@ fun AddNoticeEvent() {
     }
 
     val getNoticeImage = rememberLauncherForActivityResult(
-                ActivityResultContracts.PickVisualMedia()
+        ActivityResultContracts.PickVisualMedia()
     ) { uri: Uri? ->
         selectedNoticeUri = uri
 
@@ -116,15 +157,36 @@ fun AddNoticeEvent() {
         day
     )
 
-    val eventViewModel:EventsViewModel= hiltViewModel()
+    val isDatePickerOpen = remember { mutableStateOf(false) }
+    val isTimePickerOpen = remember { mutableStateOf(false) }
+
+
+    if (isDatePickerOpen.value) {
+        datePickerDialog.show()
+    }
+
+
+    if (isTimePickerOpen.value) {
+        val timePickerDialog = TimePickerDialog(
+            context,
+            onTimeSetListener,
+            mCalendar.get(Calendar.HOUR_OF_DAY),
+            mCalendar.get(Calendar.MINUTE),
+            false
+        )
+        timePickerDialog.show()
+    }
+
+
+
+
+    val eventViewModel: EventsViewModel = hiltViewModel()
     val noticeCoverPhoto by eventViewModel.postDetails.collectAsStateWithLifecycle()
     var noticeImage = remember { mutableStateOf<ImagesInfo?>(null) }
 
     LaunchedEffect(noticeCoverPhoto) {
-//        coverImage=null
-
-        if (noticeCoverPhoto!=null) {
-            noticeImage.value= ImagesInfo(
+        if (noticeCoverPhoto != null) {
+            noticeImage.value = ImagesInfo(
                 type = when (noticeCoverPhoto!!.second) {
                     "video/mp4" -> "Video"
                     "image/jpeg", "image/png" -> "Image"
@@ -146,16 +208,16 @@ fun AddNoticeEvent() {
     ) {
         item {
             Text("Add Notice", fontWeight = FontWeight.Bold, fontSize = 16.sp)
-
             Spacer(modifier = Modifier.height(16.dp))
+        }
 
+        item {
             OutlinedTextField(
                 value = noticeName,
                 onValueChange = { noticeName = it },
                 label = { Text("Heading for the Notice") },
                 modifier = Modifier.fillMaxWidth()
             )
-
             Spacer(modifier = Modifier.height(16.dp))
 
             OutlinedTextField(
@@ -164,28 +226,28 @@ fun AddNoticeEvent() {
                 label = { Text("Description") },
                 modifier = Modifier.fillMaxWidth()
             )
-
             Spacer(modifier = Modifier.height(16.dp))
+        }
 
+        item {
             Button(
                 onClick = { getNoticeImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)) },
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text("Select Cover Image")
             }
-
             Spacer(modifier = Modifier.height(8.dp))
-
             selectedNoticeUri?.let { uri ->
                 DisplaySelectedImage(context, uri)
             }
-
             Spacer(modifier = Modifier.height(16.dp))
+        }
 
+        item {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 readOnly = true,
-                label = { Text("Date of Event") },
+                label = { Text("Date of Notice") },
                 value = selectedDate,
                 onValueChange = { },
                 trailingIcon = {
@@ -197,14 +259,47 @@ fun AddNoticeEvent() {
             if (isPressed) {
                 datePickerDialog.show()
             }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
 
+        item {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                readOnly = true,
+                label = { Text("Time of Notice") },
+                value = selectedTime,
+                onValueChange = { },
+                trailingIcon = {
+                    Icon(Icons.Default.Schedule, contentDescription = "Select Time")
+                },
+                interactionSource = interactionSource
+            )
+
+            if (isPressed) {
+
+                val timePickerDialog = TimePickerDialog(
+                    context,
+                    onTimeSetListener,
+                    mCalendar.get(Calendar.HOUR_OF_DAY),
+                    mCalendar.get(Calendar.MINUTE),
+                    false
+                )
+                timePickerDialog.show()
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+
+
+        item {
             Button(
                 onClick = {
-                    noticesViewModel.addNotice( noticeName,
+                    noticesViewModel.addNotice(
+                        noticeName,
                         description,
                         selectedDate,
-                        )
-                 },
+                        selectedTime
+                    )
+                },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp),
@@ -213,9 +308,11 @@ fun AddNoticeEvent() {
             ) {
                 Text("Submit")
             }
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
+
 
 @Composable
 fun DisplaySelectedImage(context: Context, uri: Uri) {
@@ -248,3 +345,4 @@ fun convertUriToByteArray(context: Context, uri: Uri): ByteArray {
 
     return byteArrayOutputStream.toByteArray()
 }
+
